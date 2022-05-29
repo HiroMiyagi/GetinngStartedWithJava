731fbd6090f0d97c08bf12b17a2fee020516e8ce
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-05-29 20:33:05 +0900

    chapter3

---
+++ src/chapter3/Practice3_3.java
@@ -0,0 +1,17 @@
+package chapter3;
+
+public class Practice3_3 {
+	
+	public static void main(String[] args) {
+		int isHungry = 1;
+		String food = "バナナ";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");			
+		} else {
+			System.out.println("はらぺこです");			
+			System.out.println(food + "をいただきます");			
+			System.out.println("ごちそうさまでした");					
+		}
+	}
+}
