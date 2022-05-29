d69a98a39bd1aca9abcad4bb1e8460bbc26389b5
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-05-29 14:55:43 +0900

    chapter1～2

---
+++ src/chapter1/Practice1_3.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Practice1_3 {
+
+	public static void main(String[] args) {
+		boolean isError = true;
+		char kakeru = '駆';
+		double pi = 3.14;
+		long longNum = 314159265853979L;
+		String attackMinato = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+
+}
---
+++ src/chapter2/Practice2_1.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Practice2_1 {
+	
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int ans = x + y;
+		System.out.println("x+yは" + ans);
+		
+	}
+}
