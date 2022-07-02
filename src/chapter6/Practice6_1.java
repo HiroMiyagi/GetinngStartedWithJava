66f885e1bd1b5be4c5205537ca095b12e46fad7b
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-07-02 15:25:58 +0900

    chapter6

---
+++ src/chapter6/Practice6_1.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+import comment.Zenhan;
+
+public class Practice6_1 {
+	  public static void main(String[] args) throws Exception {
+	    Zenhan.doWarusa();
+	    Zenhan.doTogame();
+	    comment.Kouhan.callDeae();
+	    comment.Kouhan.showMondokoro();
+	  }
+}
