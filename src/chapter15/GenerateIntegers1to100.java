09d2d2b40cfdb803e769459b7b47119e98060084
HiroMiyagi <on89.miyagi.hirokatsu@gmail.com>
2022-08-01 23:49:53 +0900

    chapter15_15-1

---
+++ src/chapter15/GenerateIntegers1to100.java
@@ -0,0 +1,20 @@
+package chapter15;
+
+public class GenerateIntegers1to100 {
+    
+    public static void main (String[] args) {
+        StringBuilder sb = new StringBuilder ();
+        for (int i = 0; i <= 100; i++) {
+            sb.append (i + ",");
+        }
+        String s = sb.toString ();
+        System.out.println (s);
+        
+        String[] a = s.split (",");
+        
+        for (int i = 0; i < a.length; i++) {
+            System.out.println (a[i]);
+        }
+    }
+    
+}
